4b93d09dfc73d360dce2dbce627b690dae315f03
0b11001111 <19192307+0b11001111@users.noreply.github.com>
2020-01-23 12:19:49 +0100

    test helpers

--- autograde/__init__.py
+++ autograde/__init__.py
@@ -1,1 +1,9 @@
+# Standard library modules.
+
+# Third party modules.
+
+# Local modules
 from autograde.notebook_test import NotebookTest
+from autograde.helpers import assert_equal, assert_is, assert_isclose, assert_raises
+
+# Globals and constants variables.
---
+++ autograde/helpers.py
@@ -0,0 +1,40 @@
+# Standard library modules.
+import math
+from contextlib import contextmanager
+
+# Third party modules.
+
+# Local modules
+
+# Globals and constants variables.
+
+
+def _msg(x, exp):
+    return f'got "{str(x):.50}", expected "{str(exp):.50}"'
+
+
+def assert_equal(x, exp, msg=None):
+    assert x == exp, msg or _msg(x, exp)
+
+
+def assert_is(x, exp, msg=None):
+    assert x is exp, msg or _msg(x, exp)
+
+
+def assert_isclose(x, exp, msg=None):
+    assert math.isclose(x, exp), msg or _msg(x, exp)
+
+
+@contextmanager
+def assert_raises(*exceptions):
+    failed = False
+    exceptions = exceptions or (Exception,)
+
+    try:
+        yield None
+
+    except exceptions:
+        failed = True
+
+    assert failed, f'none of {exceptions} were risen'
+
