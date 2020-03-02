86d151739e3e57200ffebafc89ac4244e637f097
0b11001111 <19192307+0b11001111@users.noreply.github.com>
2020-03-02 12:50:13 +0100

    version 0.1.0

--- autograde/__init__.py
+++ autograde/__init__.py
@@ -7,4 +7,4 @@ from autograde.notebook_test import NotebookTest
 from autograde.helpers import assert_equal, assert_is, assert_isclose, assert_raises
 
 # Globals and constants variables.
-__version__ = '0.0.0'
+__version__ = '0.1.0'
