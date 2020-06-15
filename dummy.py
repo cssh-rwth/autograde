597372e6e151f8e8e2a752a55299ffa769f32ffb
0b11001111 <19192307+0b11001111@users.noreply.github.com>
2020-06-15 19:32:05 +0200

    more verbose output for dummy script

--- dummy.py
+++ dummy.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # Standard library modules.
-import os
 import sys
 import subprocess
 
@@ -14,10 +13,13 @@ from autograde.util import project_root
 
 
 if __name__ == '__main__':
-    print(f'autograde version {autograde.__version__}\n', flush=True)
+    print(sys.version)
+    print(f'autograde version {autograde.__version__}', flush=True)
 
     if 'unittest' in list(map(str.lower, sys.argv)):
         test_dir = project_root().joinpath('test')
         sys.exit(subprocess.run(['python', '-m', 'nose', str(test_dir)]).returncode)
+    else:
+        print('append `unittest` command to execute tests within the container')
 
     sys.exit(subprocess.run(['python', '-m', 'autograde', 'test', '--help']).returncode)
