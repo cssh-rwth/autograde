047d603f14034b5e73dfb42bcf04123d0ba5ed45
0b11001111 <19192307+0b11001111@users.noreply.github.com>
2021-02-10 10:55:51 +0100

    simplify dummy script

--- dummy.py
+++ dummy.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-import subprocess
 import sys
 
-import autograde
+from autograde.cli import cli
 
 if __name__ == '__main__':
-    print(sys.version)
-    print(f'autograde version {autograde.__version__}', flush=True)
-    sys.exit(subprocess.run(['python', '-m', 'autograde', '--help']).returncode)
+    cli(['version'])
+    print(flush=True)
+    cli(['--help'])
+    sys.exit(0)
